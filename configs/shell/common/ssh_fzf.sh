355a05a0069ed7b1dc2acbc2826c52d82254b28c
solerbus <simon.buergi@lernende.so.ch>
2025-06-06 11:06:38 +0200

    new common for ssh with fzf

---
+++ configs/shell/common/ssh_fzf.sh
@@ -0,0 +1,14 @@
+# Wrapper for SSH with fzf integration
+ssh() {
+  if [ $# -eq 0 ]; then
+    # Extract all Host entries (excluding wildcards and patterns)
+    local host=$(awk '/^Host / { for (i=2; i<=NF; i++) print $i }' ~/.ssh/config | grep -v '[*?]' | fzf --prompt="SSH to > ")
+    if [ -n "$host" ]; then
+      command ssh "$host"
+    fi
+  else
+    # Call original ssh with arguments
+    command ssh "$@"
+  fi
+}
+
