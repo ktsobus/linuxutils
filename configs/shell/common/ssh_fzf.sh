6f162a8f7b142f93f816c42475459dad0d9be3ec
solerbus <simon.buergi@lernende.so.ch>
2025-06-10 11:36:39 +0200

    ui edit

--- configs/shell/common/ssh_fzf.sh
+++ configs/shell/common/ssh_fzf.sh
@@ -2,7 +2,7 @@
 ssh() {
   if [ $# -eq 0 ]; then
     # Extract all Host entries (excluding wildcards and patterns)
-    local host=$(awk '/^Host / { for (i=2; i<=NF; i++) print $i }' ~/.ssh/config | grep -v '[*?]' | fzf --prompt="SSH to > " --height=60% )
+    local host=$(awk '/^Host / { for (i=2; i<=NF; i++) print $i }' ~/.ssh/config | grep -v '[*?]' | fzf --prompt="SSH to > " --height=75% )
     if [ -n "$host" ]; then
       command ssh "$host"
     fi
