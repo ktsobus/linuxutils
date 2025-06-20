ad6f84b500850a3d96dea15eca46015056cb8fff
solerbus <simon.buergi@lernende.so.ch>
2025-06-20 09:46:06 +0200

    small fuzzygrep change

--- configs/shell/common/fuzzygrep.sh
+++ configs/shell/common/fuzzygrep.sh
@@ -6,8 +6,8 @@ fuzzygrep() {
   fi
 
   local file
-  file=$(rg --files-with-matches --no-heading --color=never "$query" | \
-         fzf --preview "rg --color=always --line-number '$query' {}" \
+  file=$(rg --smart-case --hidden --files-with-matches --no-heading --color=never "$query" | \
+         fzf --preview "rg --smart-case --hidden --color=always --line-number '$query' {}" \
              --prompt="Datei auswählen: " \
              --preview-window=up:70% \
              --layout=reverse)
@@ -15,9 +15,9 @@ fuzzygrep() {
   [ -z "$file" ] && return
 
   local selection
-  selection=$(rg --line-number --no-heading --color=never "$query" "$file" | \
+  selection=$(rg --smart-case --hidden --line-number --no-heading --color=never "$query" "$file" | \
               fzf --prompt="Treffer auswählen: " \
-                  --preview 'n={1}; low=$((n-7)); [ $low -lt 1 ] && low=1; high=$((n+7)); batcat --style=numbers --color=always --highlight-line $n --line-range $low:$high '"\"$file\"" \
+                  --preview 'n={1}; low=$((n-7)); [ $low -lt 1 ] && low=1; high=$((n+7)); bat --style=numbers --color=always --highlight-line $n --line-range $low:$high '"\"$file\"" \
                   --preview-window=up:70% \
                   --delimiter=: --with-nth=1,2..)
 
@@ -30,7 +30,7 @@ fuzzygrep() {
   linecontent=$(sed "${linenr}q;d" "$file")
 
   local column
-  column=$(awk -v l="$linecontent" -v q="$query" 'BEGIN{print index(l,q)}')
+  column=$(awk -v IGNORECASE=1 l="$linecontent" -v q="$query" 'BEGIN{print index(l,q)}')
 
   if [ "$column" -gt 0 ]; then
     ${EDITOR:-vim} +"call cursor($linenr,$column)" "$file"
