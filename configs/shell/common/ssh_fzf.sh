eb9584432e8f58a5762e4562b21075c2fa940e0f
solerbus <simon.buergi@lernende.so.ch>
2025-08-06 09:43:33 +0200

    update ss fzf

--- configs/shell/common/ssh_fzf.sh
+++ configs/shell/common/ssh_fzf.sh
@@ -4,7 +4,15 @@ ssh() {
     # Extract all Host entries (excluding wildcards and patterns)
     local host=$(awk '/^Host / { for (i=2; i<=NF; i++) print $i }' ~/.ssh/config | grep -v '[*?]' | sort | fzf --prompt="SSH to > " --height=75% )
     if [ -n "$host" ]; then
+
+      if [[ -n "$ZSH_VERSION" ]]; then
+        print -s "ssh $host"
+      else
+        history -s "ssh $host"
+      fi
+
       command ssh "$host"
+
     fi
   else
     # Call original ssh with arguments
