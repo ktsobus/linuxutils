a157f4932b9f29568b2165759c3f61a7411b85fd
ktsobus <simon.buergi@aio.so.ch>
2026-08-26 08:19:24 +0200

    new alias

--- configs/shell/common/aliases.sh
+++ configs/shell/common/aliases.sh
@@ -30,3 +30,6 @@ alias lg='lazygit'
 
 # network
 alias unset-proxys='unset HTTP_PROXY HTTPS_PROXY NO_PROXY http_proxy https_proxy no_proxy ALL_PROXY all_proxy'
+
+# ai
+alias claude='unset-proxys && claude'
